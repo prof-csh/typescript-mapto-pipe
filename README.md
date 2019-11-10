9b0c9d2fc22bd544ed40ed2dc19e71d10295460a
prof-csh <prof.csh@gmail.com>
2019-11-09 18:43:08 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-d6x25k
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-d6x25k)
---
+++ index.ts
@@ -0,0 +1,18 @@
+// Import stylesheets
+import './style.css';
+
+import { of } from 'rxjs/observable/of';
+import { merge } from 'rxjs/observable/merge';
+import { mapTo, delay } from 'rxjs/operators';
+
+//emit one item
+const example = of(null);
+//delay output of each by an extra second
+const message = merge(
+  example.pipe(mapTo('Hello')),
+  example.pipe(mapTo('World!'), delay(1000)),
+  example.pipe(mapTo('Goodbye'), delay(2000)),
+  example.pipe(mapTo('World!'), delay(3000))
+);
+//output: 'Hello'...'World!'...'Goodbye'...'World!'
+const subscribe = message.subscribe(val => console.log(val));
